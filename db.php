ce4959ea935a25dfb01252de0f82bbb60664f7b2
zaky06974 <146085868+MuhamadZakyHannan@users.noreply.github.com>
2025-04-23 15:07:20 +0700

    first commit

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "kampus";
+$db = "echo " # uts_pwel" >> README.md";
 $koneksi = new mysqli($host, $user, $pass, $db);
 if ($koneksi->connect_error) {
   die("Koneksi gagal: " . $koneksi->connect_error);
